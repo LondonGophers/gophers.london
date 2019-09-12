ef599c8f4c807745cf47524828cc77ce9afa453d
Paul Jolly <paul@myitcv.io>
2019-09-12 10:30:27 +0100

    deps: upgrade to use Go 1.13 by default (#20)

--- _scripts/run_docker_run.sh
+++ _scripts/run_docker_run.sh
@@ -2,4 +2,4 @@
 
 set -e
 
-docker run --rm -v $PWD:/gophers.london -w /gophers.london --user $(id -u):$(id -g) golang:1.12.8 ./_scripts/docker_run.sh
+docker run --rm -v $PWD:/gophers.london -w /gophers.london --user $(id -u):$(id -g) golang:1.13 ./_scripts/docker_run.sh
