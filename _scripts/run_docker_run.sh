8a489a28fe08f8154eb1de9e4361e36ab3273344
Dominic Green <dominicgreen1@gmail.com>
2019-09-12 10:47:51 +0100

    update logo (#19)

--- _scripts/run_docker_run.sh
+++ _scripts/run_docker_run.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
-set -e
+set -eu
 
-docker run --rm -v $PWD:/gophers.london -w /gophers.london --user $(id -u):$(id -g) golang:1.13 ./_scripts/docker_run.sh
+# If we have a single-element GOPATH, map it and set an env var
+mapping="-v $(go env GOCACHE):/gocache --env GOCACHE=/gocache"
+gp=$(go env GOPATH)
+if echo $gp | grep ':'
+then
+	echo "Multiple-element GOPATH set: $gp. Not mounting in Docker container"
+else
+	mapping="-v $gp:/gopath --env GOPATH=/gopath $mapping"
+fi
+
+docker run --rm $mapping -v $PWD:/gophers.london -w /gophers.london --user $(id -u):$(id -g) golang:1.13 ./_scripts/docker_run.sh
